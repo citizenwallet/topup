a9d2e654f729e80b981b65e93f40579ba44fbbfd
Xavier Damman <xdamman@gmail.com>
2024-01-02 02:54:33 +0100

    clean

--- src/lib/account.js
+++ src/lib/account.js
@@ -11,10 +11,6 @@ import AccountFactoryAbi from "@/smartcontracts/AccountFactory.abi.json";
 const AccountFactoryAddress = process.env.NEXT_PUBLIC_ACCOUNT_FACTORY_ADDRESS;
 
 export async function getAccountForWalletAddress(walletAddress) {
-  console.log(
-    ">>> process.env.POLYGON_MUMBAI_RPC_URL",
-    process.env.POLYGON_MUMBAI_RPC_URL
-  );
   console.log(">>> get account for wallet address", walletAddress);
 
   const provider = new ethers.providers.JsonRpcProvider({
