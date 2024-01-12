99363a29d581e93998917526e0fa8e3000a48200
kev-techi <kevin@techi.be>
2024-01-12 18:02:01 +0100

    display the faucet's account address

--- src/components/CommunityCard.jsx
+++ src/components/CommunityCard.jsx
@@ -12,8 +12,8 @@ import { Button } from "@/components/ui/button";
 import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-function useFaucet(account, communitySlug) {
-  const apicall = `/api/balance?account=${account}&communitySlug=${communitySlug}`;
+function useFaucet(communitySlug) {
+  const apicall = `/api/balance?communitySlug=${communitySlug}`;
   const { data, error, isLoading } = useSWR(apicall, fetcher);
   return {
     faucet: data,
@@ -38,10 +38,7 @@ const networks = {
 };
 
 export default function CommunityCard({ community }) {
-  const { faucet, isLoading, isError } = useFaucet(
-    process.env.NEXT_PUBLIC_FAUCET_ADDRESS,
-    community.slug
-  );
+  const { faucet, isLoading, isError } = useFaucet(community.slug);
 
   return (
     <Card className="w-full max-w-md mb-6">
@@ -49,33 +46,45 @@ export default function CommunityCard({ community }) {
         <CardTitle>{community.name}</CardTitle>
         <div className="text-sm text-gray-500">{community.formattedFxRate}</div>
       </CardHeader>
-      <CardContent className="flex flex-col">
-        <div className="flex flex-col my-2">
-          <label className="mr-1">Faucet address:</label>
-          <div className="text-xs text-gray-500">
-            <a
-              href={`${community.config.scan.url}/address/${process.env.NEXT_PUBLIC_FAUCET_ADDRESS}`}
-            >
-              {process.env.NEXT_PUBLIC_FAUCET_ADDRESS}
-            </a>
+      {faucet && (
+        <CardContent className="flex flex-col">
+          <div className="flex flex-col my-2">
+            <label className="mr-1">Faucet address:</label>
+            <div className="text-xs text-gray-500">
+              <a
+                href={`${community.config.scan.url}/address/${faucet.address}`}
+              >
+                {faucet.address}
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col">
-          <label className="mr-1">Balance:</label>
-          <div className="text-xs">
-            {!isLoading && !isError && faucet && (
-              <div className="text-sm text-gray-500">
-                <div>
-                  {faucet.nativeBalance} {networks[faucet.chainId].symbol}
-                </div>
-                <div>
-                  {faucet.balance} {faucet.tokenSymbol}
-                </div>
-              </div>
-            )}
+          <div className="flex flex-col my-2">
+            <label className="mr-1">Faucet account address:</label>
+            <div className="text-xs text-gray-500">
+              <a
+                href={`${community.config.scan.url}/address/${faucet.account}`}
+              >
+                {faucet.account}
+              </a>
+            </div>
           </div>
-        </div>
-      </CardContent>
+          <div className="flex flex-col">
+            <label className="mr-1">Balance:</label>
+            <div className="text-xs">
+              {!isLoading && !isError && faucet && (
+                <div className="text-sm text-gray-500">
+                  <div>
+                    {faucet.nativeBalance} {networks[faucet.chainId].symbol}
+                  </div>
+                  <div>
+                    {faucet.balance} {faucet.tokenSymbol}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </CardContent>
+      )}
       <CardFooter>
         <Link
           className="border border-gray-300 rounded-md p-3 dark:border-gray-600 block text-center py-2"
