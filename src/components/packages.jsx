970d290135a9cc21e1918d739b446f9c3ac013ba
Xavier Damman <xdamman@gmail.com>
2024-09-21 20:37:41 +0200

    fix bug for top up EURb

--- src/components/packages.jsx
+++ src/components/packages.jsx
@@ -126,10 +126,7 @@ export function Packages({
             isItemLoading === pkg.key && "packageButtonLoading"
           }`}
           onClick={() =>
-            !loading &&
-            !isItemLoading &&
-            pkg.amount <= faucetBalance &&
-            handleClick(pkg.buyUrl, pkg.key)
+            !loading && !isItemLoading && handleClick(pkg.buyUrl, pkg.key)
           }
         >
           <div className="text-purple-primary flex flex-row items-center mx-auto">
